1d1b2626236d594e6c6a3a040f2f9a1ddc1d07ae
ianicno <ianicno@gmail.com>
2014-11-14 11:40:48 +0700

    level

---
+++ index2.php
@@ -0,0 +1,7 @@
+<form method="post" action="login.php" class="form-horizontal">
+	<label class="username">Username</label>
+	<input type="text" name="username" placeholder="Username Anda">
+	<label class="password">Password</label>    	
+	<input type="password" name="pass" placeholder="Password">
+	<button type="submit" name="submit" class="btn">Login</button>
+</form>
